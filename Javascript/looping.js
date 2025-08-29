8f38d862a21a0110dbe92ad347f2a51017e679f7
pranto-islam <prantoislamnt51@gmail.com>
2025-08-29 20:40:35 +0600

    jh

---
+++ Javascript/looping.js
@@ -0,0 +1,8 @@
+
+
+
+
+let number = 10;
+for (number; number < 100; number++){
+    console.log("Hellow", number);
+}
---
+++ Javascript/variable_print.js
@@ -0,0 +1,7 @@
+
+
+
+let my = 10;
+const number = 10;
+
+console.log(my+number);
