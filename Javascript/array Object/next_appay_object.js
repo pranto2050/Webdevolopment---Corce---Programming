248b348ecb6942f7ba67fd3cdeb685cdf24015db
pranto-islam <prantoislamnt51@gmail.com>
2025-09-10 18:00:39 +0600

    Test for Upper Case

--- Javascript/array Object/next_appay_object.js
+++ Javascript/array Object/next_appay_object.js
@@ -9,17 +9,37 @@ let persone = {
     name: "Md.Pranto Islam",
     age: 32,
     mobile: "01979728818",
-    color: ["red","yellow","green","blue"]
+    color: ["red","yellow","green","blue"],
+    roll: 30
 }
 
-console.log(persone.color[1])
+
+
+// Object Looping
+for(let person in persone){
+    // console.log(persone[person]);
+}
+
+
+
+// console.log(persone.color[1])
 
 // Fine Key and Value
 
 console.log(Object.keys(persone));
 console.log(Object.values(persone));
+// console.log(Object.values(persone));
+
+// for (let i = 0; i < length; i++) {}
 
 
+
+console.log(persone.color)
+let len = persone.color.length
+console.log(len)
+for (let i = 0; i < len; i++) {
+    console.log(persone.color[i].toUpperCase());
+}
 
 
 
