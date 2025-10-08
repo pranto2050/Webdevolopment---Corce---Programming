6ea24b562a79ab823de9fc89d4c4bf08fd065a90
pranto-islam <prantoislamnt51@gmail.com>
2025-10-08 21:21:57 +0600

    New Push

---
+++ Javascript/Mileston 5/25-2 Traversing dom/25-2 Traversing dom.js
@@ -0,0 +1,76 @@
+
+
+
+
+console.log("Pranto");
+
+
+
+let student = {
+    name: `John`,
+    id: 2112,
+    section: `A`,
+    corce: 233,
+    stydy: function(itme){
+        console.log(`Student class time is`,itme);
+    }
+};
+
+
+
+let get_data = document.getElementById("email_section")
+console.log(get_data.innerText);
+
+let pute = document.getElementById("adddedd");
+pute.innerText = `<h1> Hellow World </h1>`;
+
+
+
+let classes = document.getElementsByClassName("home_section");
+// Give collection of Array
+console.log(classes);
+// for (let list of classes){
+//     // All List
+//     console.log(list);
+//     // Inner Text
+//     console.log(list.innerText);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/Mileston 5/25-4 Traversing dom/index.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+// Take dat using id or class acoding class name and ID
+console.log(`Hi`);
+// Use ID take all
+// console.log(document.querySelectorAll("#listts"));
+// // Use Class
+// console.log(document.querySelectorAll(".unoder_list li"));
+//
+//
+//
+// // Use Variable
+// let sss = document.querySelectorAll(".unoder_list li");
+// console.log(sss);
+//
+// for(let list of sss){
+//     console.log(list);
+//     console.log(list.innerText);
+// }
+
+
+
+// Take a single data
+console.log(document.querySelector(".unoder_list li"));
